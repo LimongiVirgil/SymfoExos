52535c9ce3196c9c3b07ecae12acd825196664f6
LimongiVirgil <limongi.virgil@gmail.com>
2021-02-18 14:55:22 +0100

    add beers

--- bar-tp-perso/src/Controller/BarController.php
+++ bar-tp-perso/src/Controller/BarController.php
@@ -6,8 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
 class BarController extends AbstractController
 {
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    private function beers_api(): Array
+    {
+        $response = $this->client->request(
+            'GET',
+            'https://raw.githubusercontent.com/Antoine07/hetic_symfony/main/Introduction/Data/beers.json'
+        );
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+        $content = $response->getContent();
+        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+        $content = $response->toArray();
+        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+
+        return $content ;
+    }
+
     /**
      * @Route("/bar", name="bar")
      */
@@ -36,6 +62,7 @@ class BarController extends AbstractController
     {
         return $this->render('beers/index.html.twig', [
             'title' => 'Beers',
+            'beers' => $this->beers_api(),
         ]);
     }
 }
